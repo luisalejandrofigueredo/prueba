ed5cc6a8a6fd7a6dd44a956cdfa62363a69f2387
Luis Figueredo <luisalejandrofigueredo@gmail.com>
2024-08-13 22:24:01 -0300

    listo

--- src/app/table-skills/table-skills.component.ts
+++ src/app/table-skills/table-skills.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule, MatPaginator ,MatPaginatorIntl} from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
 import { TableSkillsDataSource, TableSkillsItem } from './table-skills-datasource';
 import { NgOptimizedImage } from '@angular/common'
@@ -11,6 +11,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
   templateUrl: './table-skills.component.html',
   styleUrl: './table-skills.component.scss',
   standalone: true,
+  providers:[MatPaginatorIntl],
   imports: [MatTableModule, MatPaginatorModule, MatSortModule,NgOptimizedImage,MatIconModule,MatTooltipModule]
 })
 export class TableSkillsComponent implements AfterViewInit {
@@ -21,7 +22,21 @@ export class TableSkillsComponent implements AfterViewInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['image','name','link'];
-
+  constructor(private pag: MatPaginatorIntl){
+    pag.itemsPerPageLabel = $localize `Items per page`;
+    pag.firstPageLabel = $localize `First page`;
+    pag.nextPageLabel = $localize `Next page`;
+    pag.previousPageLabel = $localize `Previous page`;
+    pag.getRangeLabel = (page: number, pageSize: number, length: number) => {
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex =
+        startIndex < length
+          ? Math.min(startIndex + pageSize, length)
+          : startIndex + pageSize;
+      return `${startIndex + 1}`+ $localize `of`+ `${endIndex} / ${length}`;
+    }
+  }
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
